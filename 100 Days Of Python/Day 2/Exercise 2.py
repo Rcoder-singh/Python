c75acfe85825dfbeb7d20731dfec48b68ecceafa
Rcoder-singh <rishabh22.singh@gmail.com>
2021-05-20 20:20:31 +0530

    Calculate BMI

---
+++ 100 Days Of Python/Day 2/Exercise 2.py
@@ -0,0 +1,10 @@
+#Calculate BMI
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+BMI = int(int(weight)/float(height))
+print(BMI)
