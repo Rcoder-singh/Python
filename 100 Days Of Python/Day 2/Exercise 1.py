137a7744461cab7e8b2421b7f259e04bab026696
Rcoder-singh <rishabh22.singh@gmail.com>
2021-05-20 20:15:48 +0530

    Input a number and and the digits and print result

---
+++ 100 Days Of Python/Day 2/Exercise 1.py
@@ -0,0 +1,15 @@
+# Input a number and and the digits and print result
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#Check the data type of two_digit_number
+print(type(two_digit_number))
+#Get the first and second digits using subscripting then convert string to int.
+#Add the two digits togeter
+
+print(int(two_digit_number[0])+int(two_digit_number[1]))
+  
