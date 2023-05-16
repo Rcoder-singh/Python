b179a5e7e407808e1a24ccbec2091e756a21f24d
Rishabh Singh <rishabh22.singh@gmail.com>
2023-05-17 00:04:41 +0530

    Function parameters

---
+++ 100 Days Of Python/Day 8/Exercise 1.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+import math 
+def paint_calc(height,width,cover):
+    area = height *width
+    num_of_cans = math.ceil(area/cover)
+    print(f"You'll need {num_of_cans} cans of paint.")
+
+
+
+
+#Write your code above this line 👆
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height =test_h, width = test_w, cover = coverage)
